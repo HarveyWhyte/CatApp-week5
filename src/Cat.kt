c9a1bc94f5281cf9a308045a22bce90f15dc8176
Harvey Whyte <0whyth98@solent.ac.uk>
2025-10-20 15:30:05 +0100

    fix another bug in walk()

--- src/Cat.kt
+++ src/Cat.kt
@@ -4,7 +4,7 @@ class Cat (val name: String, var age: Int, var weight: Int) {
     // Should check if the weight is 5 or more
     // There is a bug - don't resolve until instructed by the exercise.
     fun walk(distance: Int) :Boolean {
-        if(weight <= distance + 5) {
+        if(weight <= distance + 6) {
             weight = weight - distance
             return true
         }
